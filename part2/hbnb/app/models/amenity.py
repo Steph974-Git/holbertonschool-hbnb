32a909f47013200261575802cfe4a3bd19dd1d11
FontGuillaume <guillaume_font@orange.fr>
2025-06-16 14:29:29 +0200

    Supprimer les fichiers Base.py et Place.py, ajout de la classe Amenity avec validation des attributs et mise à jour de BaseModel et Place pour inclure des vérifications supplémentaires.

---
+++ part2/hbnb/app/models/amenity.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+from app.models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    def __init__(self, name):
+        super().__init__()
+        if not name:
+            raise ValueError("Name is required")
+        if len(name) > 50:
+            raise ValueError("Name must be 50 characters maximum")
+        
+        self.name = name

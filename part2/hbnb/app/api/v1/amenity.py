01a47437360c3bb50ce275e5ada89056ea7c5bff
FontGuillaume <guillaume_font@orange.fr>
2025-06-17 14:35:02 +0200

    Améliore la gestion des amenities : ajoute la récupération et la mise à jour avec des réponses appropriées

--- part2/hbnb/app/api/v1/amenity.py
+++ part2/hbnb/app/api/v1/amenity.py
@@ -35,7 +35,19 @@ class AmenityList(Resource):
     def get(self):
         """Retrieve a list of all amenities"""
         # Récupère toutes les amenities
-        hbnb_facade = f
+        hbnb_facade = facade.HBnBFacade()
+        amenities = hbnb_facade.get_all_amenities()
+
+        #Si aucune amenity n'existe, retourne une liste vide
+        if not amenities:
+            return [], 200
+        
+        # Formate la réponse
+        result = []
+        for amenity in amenities:
+            result.append({'id': amenity.id, 'name': amenity.name})
+        return result, 200
+    
 
 @api.route('/<amenity_id>')
 class AmenityResource(Resource):
@@ -43,8 +55,16 @@ class AmenityResource(Resource):
     @api.response(404, 'Amenity not found')
     def get(self, amenity_id):
         """Get amenity details by ID"""
-        # Placeholder for the logic to retrieve an amenity by ID
-        pass
+        hbnb_facade = facade.HBnBFacade()
+        amenity = hbnb_facade.get_amenity_by_id(amenity_id)
+
+        # Vérifie si l'amenity existe
+        if not amenity:
+            return {"message": f"Amenity with ID {amenity_id} not found"}, 404
+        
+        # Retourne les details de l'amenity
+        return {'id': amenity.id, 'name': amenity.name}, 200
+
 
     @api.expect(amenity_model)
     @api.response(200, 'Amenity updated successfully')
@@ -52,5 +72,24 @@ class AmenityResource(Resource):
     @api.response(400, 'Invalid input data')
     def put(self, amenity_id):
         """Update an amenity's information"""
-        # Placeholder for the logic to update an amenity by ID
-        pass
+        # Récupère les données de la requête
+        data = api.payload
+
+        # Validation données
+        if not data.get('name'):
+            return {"message": f"Amenity with ID {amenity_id} not found"}, 404
+        if len(data['name']) > 50:
+            return {"message": "Name must be less than 50 characters"}, 400
+        
+        # Vérifie d'abord si l'amenity existe
+        hbnb_facade = facade.HBnBFacade()
+        existing_amenity = hbnb_facade.get_amenity_by_is(amenity_id)
+
+        if not existing_amenity:
+            return {"message": f"Amenity with ID {amenity_id} not found"}, 404
+        
+        # Met a jour l'amenity
+        updated_amenity = hbnb_facade.update_amenity(amenity_id, data['name'])
+
+        # Retourne l'amenity mise à jour
+        return {'id': updated_amenity.id, 'name': updated_amenity.name}, 200

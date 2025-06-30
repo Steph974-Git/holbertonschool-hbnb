e2fcd5df4861e8cfad405551c0023832266fb828
Steph974-Git <10466@holbertonstudents.com>
2025-06-30 15:02:15 +0200

    Add JWT configuration to the app and update requirements

--- part3/hbnb/config.py
+++ part3/hbnb/config.py
@@ -3,6 +3,8 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    JWT_SECRET_key = os.getenv('JWT_SECRET_KEY', 'default_jwt_secret_key')
+    JWT_ACCESS_TOKEN_EXPIRES 
     DEBUG = False
 
 

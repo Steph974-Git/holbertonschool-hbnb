e69feca515a7a6ca40c4414d8f830fe97eb90ad9
Steph974-Git <10466@holbertonstudents.com>
2025-07-02 09:44:31 +0200

    Reorganize imports in __init__.py for clarity; move API namespace imports below app initialization

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -2,11 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_restx import Api
-from app.api.v1.users import api as users_ns
-# Importation du namespace des amenities
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.reviews import api as reviews_ns
 from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
@@ -26,6 +21,12 @@ def create_app(config_class="config.DevelopmentConfig"):
     # Placeholder for API namespaces (endpoints will be added later)
     # Additional namespaces for places, reviews, and amenities will be added
     # later
+
+    from app.api.v1.users import api as users_ns
+    # Importation du namespace des amenities
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.places import api as places_ns
+    from app.api.v1.reviews import api as reviews_ns
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')

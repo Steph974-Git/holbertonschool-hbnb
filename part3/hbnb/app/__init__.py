6e72694afd207e4af8b9c4a8fdcbc2da1cbf0e92
FontGuillaume <guillaume_font@orange.fr>
2025-07-02 11:27:24 +0200

    Ajouter le namespace d'authentification à l'API

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -27,6 +27,8 @@ def create_app(config_class="config.DevelopmentConfig"):
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.places import api as places_ns
     from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.auth import api as auth_ns
+    
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
@@ -34,4 +36,5 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/vi/auth')
     return app
